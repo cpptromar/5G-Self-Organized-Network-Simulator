d656af126f37aec526a77d3a55c76fa21a4de366
Jarod T. Ma <jtma@cpp.edu>
2020-11-05 23:28:50 -0800

    updates to user mobility - added mobility type - added usermobility() function - changed around IRP Manager

--- SHNSim/IRPManager.cpp
+++ SHNSim/IRPManager.cpp
@@ -89,6 +89,8 @@ void IRPManager::IRPManagerUpdate()
 	{
 		IRPManager::dataAnalysis();
 
+		IRPManager::mobileuser(); //Simulates users moving around from one base station to another
+
 		// Self-Healing functions
 		IRPManager::checkStatus();
 		IRPManager::offloadUser();
@@ -126,6 +128,7 @@ void IRPManager::IRPDataCollection()
 						tr.getLoc().y,
 						tr.getTheta(),
 						ueTrPair.first,
+						usr.getMobilityID(),
 						usr.getLoc().x,
 						usr.getLoc().y,
 						usr.getMaxDr(),
@@ -267,6 +270,82 @@ void IRPManager::offloadUser()
 				removalAttemptFailed++;
 				ErrorTracer::error("Error transferring UE to new eNodeB");
 			}
+		}
+	}
+}
+
+void IRPManager::mobileuser()
+{
+// calculate max distance to search for												//Take into consideration the user's mobility type
+float maxSearchDist = 4.5f * Simulator::getBSRegionScalingFactor();
+
+// for each eNodeB that needs help
+for (const auto& unhealthyBS : disabledBSs)
+{
+
+	// get the list of users stored within a disabled eNodeB
+	const auto& disabledBsRecords = Simulator::getBS(unhealthyBS).getUEDB();
+	const auto& healthyBsRecords = Simulator::getBS(hbs).getUEDB();
+
+	//Create temporary vector to hold information about UE demand
+	auto users = std::vector<size_t>();
+	
+	users.reserve(combinedRecords.size());
+	for (const auto& ue : combinedRecords.readDB())
+		users.push_back((*ue).userID);
+
+
+	//should prevent while loop from running indefinitely
+	auto amountToRemove = uint32_t{ 5 };											//This many users are mobile (To be replaced with Simulator::AP_IRPNumMobileUsers or something)
+	auto prevAmountToRemove = amountToRemove;
+	auto removalAttemptFailed = uint32_t{ 0 };
+
+	while (amountToRemove > 0 && removalAttemptFailed < Simulator::AP_IRPMaxRemovalFailures)
+	{
+		// get the location and id of the first user within the User Equipment DataBase (UEDB) of the unhealthy eNodeB
+		if (combinedRecords.size() < 1)
+		{
+			removalAttemptFailed++;
+			continue;
+		}
+		const auto usrID = users.back();											//usr to remove from eNodeB
+		const auto offloadUserLoc = (*(combinedRecords.look_up(usrID))).loc;		//usr location
+		users.pop_back();
+
+		// variables for determining the closest eNodeB to the UE to be removed
+		auto minDistBetweenUEandBS = float{ 100000 };				// set it to arbitrarily large number
+		auto distBetweenUEandBS = float{};							// dist holder
+		auto closestBS_ID = size_t{ Simulator::getNumOfBSs() };		// destination to remove user to, initialized to an invalid BSID
+
+		// iterate through the list of helper eNodeBs and compare their distance to the user to be removed
+		// to determine the closest BS to offload to
+		for (const auto& hbs : this->helperBSs)
+		{
+			// Location of current eNodeB
+			const auto& helperBSLoc = Simulator::getBS(hbs).getLoc();
+
+			// Determine the distance between the UE to be removed and the current helper eNodeB			
+			distBetweenUEandBS = sqrt(((offloadUserLoc.y - helperBSLoc.y) * (offloadUserLoc.y - helperBSLoc.y)) + ((offloadUserLoc.x - helperBSLoc.x) * (offloadUserLoc.x - helperBSLoc.x)));
+
+			// Search for the closest eNodeB taking into consideration the user's mobility type
+			if (distBetweenUEandBS <= maxSearchDist && distBetweenUEandBS < minDistBetweenUEandBS)
+			{
+				closestBS_ID = hbs; // store closest BS ID to offload user too
+				minDistBetweenUEandBS = distBetweenUEandBS;
+			}
+
+		}
+
+		// Add the user to the to the closest BS and remove it from the original BS where it is at
+		if (closestBS_ID < Simulator::getNumOfBSs() && Simulator::transferUE(unhealthyBS, usrID, closestBS_ID, 0))
+		{
+			amountToRemove -= 1;
+		}
+
+		if (amountToRemove == prevAmountToRemove)
+		{
+			removalAttemptFailed++;
+			ErrorTracer::error("Error moving UE to new eNodeB");
 		}
 	}
 }
